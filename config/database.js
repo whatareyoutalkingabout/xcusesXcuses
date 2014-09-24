bb0fa2f8c4054daf9c1ff6c019a99396f356632f
whatareyoutalkingabout <greg@trackbike.com>
2014-09-24 19:31:51 -0400

    Init commit
    init commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://xcuses:forReal@ds039880.mongolab.com:39880/xcuses'//'your_database_here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
